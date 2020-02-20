daf909710bdac7f3736619aa7764cbee644b23f1
No-Max <malyavko.max@gmail.com>
2020-02-20 03:12:15 +0300

    remove unnec code + add helper methods

--- src/scripts/classes/Card.js
+++ src/scripts/classes/Card.js
@@ -1,28 +1,28 @@
+import cardHelper from '../helpers/card.js';
+
 export default class {
-    constructor({number, comment, brandImageUrl}) {
-        this.id = `card${number.replace(' ', '')}`;
+    constructor({ number, comment, brandImageUrl }) {
+        this.id = `card${cardHelper.removeSpaces(number)}`;
         this.number = number;
         this.comment = comment;
         this.brandImageUrl = brandImageUrl;
     };
+
     getAsHtml(buttonHandler) {
         const card = document.createElement('div');
         const button = document.createElement('button');
         
         card.setAttribute('class', 'list-card');
-        card.setAttribute('id', this.id);
-        card.innerHTML = `<div class="list-card__comment">${this.comment}</div>
-            <div class="list-card__number">${this.number}</div>
+        card.innerHTML = `
+            <div class="list-card__comment">${this.comment}</div>
+            <div class="list-card__number">${cardHelper.splitByFourNumbers(this.number)}</div>
             <img src="${this.brandImageUrl}"/>`;
         
         button.setAttribute('type', 'button');
-        button.innerText = 'Remove'
+        button.innerText = 'Remove';
         button.onclick = buttonHandler;
         card.append(button)
 
         return card;
-    };
-    removeButton() {
-        
     };
 }
